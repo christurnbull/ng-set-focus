14adac51c59a25a2fb4b5ad915eb778510e4b2ea
Chris Turnbull <christurnbull@users.noreply.github.com>
2016-01-09 22:50:04 +0000

    Update ngSetFocus.js
    Fixed set-focus-default bug

--- ngSetFocus.js
+++ ngSetFocus.js
@@ -12,18 +12,27 @@ angular.module('ngSetFocus', [])
   	return {
   	  restrict: 'A',
   	  link: function(scope, element, attrs) {
+        
+        // set focus when attribute variable has changed
+        attrs.$observe('ngSetFocus', function() {
+          
+          // dont set focus on load
+          if(attrs.ngSetFocus == 'true' || attrs.ngSetFocus == 'false'){
+            
+            // after other events have complete
+            $timeout(function(){
+              element[0].focus();
+            }, 1);
+
+          }
+        })
   	    
-  	    // set focus when attribute variable has changed
-  	    attrs.$observe('ngSetFocus', function() {
-  	      element[0].focus()
-  	    })
-  	    
-  	    // apply default focus after all other events have complete
+  	    // apply default focus after other events have complete
   	    $timeout(function(){
-      		if(attrs.hasOwnProperty('setFocusDefault')){
-      		  element[0].focus()
-      		}
-  		  }, 1);
+          if(attrs.hasOwnProperty('setFocusDefault')){
+            element[0].focus();
+          }
+        }, 1);
 
   	  }	  
   	};
